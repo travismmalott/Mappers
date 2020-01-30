e51af6beb5bad483d1164fedd4d0a88744ccd904
brandonarivera <56692438+brandonarivera@users.noreply.github.com>
2020-01-30 15:03:38 -0600

    Adding connection string Updated migrations and database

--- Group/Migrations/20200130195805_mappersfirst.Designer.cs
+++ Group/Migrations/20200130195805_mappersfirst.Designer.cs
@@ -4,14 +4,16 @@ using Group.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace Group.Data.Migrations
+namespace Mappers.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200130195805_mappersfirst")]
+    partial class mappersfirst
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
