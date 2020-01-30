1879ab99333dceb991a363eb035cf88828627297
Travis <travismmalott@outlook.com>
2020-01-30 13:11:37 -0600

    Travis update startup.cs

--- Group/Startup.cs
+++ Group/Startup.cs
@@ -1,6 +1,6 @@
 //Brandon
 //WHO DOES NUMBER TWO WORK FORRR?
-//Hello yellow, the one and only Lola Coka Cola :3
+//Hello yellow, the one and only Lola Coka Cola :3 Lola is my spirit animal <3 Which means Ana is my master. 
 using System;
 using System.Collections.Generic;
 using System.Linq;
