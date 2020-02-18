823ac53acd262625d3d40387b3b71a8f7539fd75
brandonarivera <56692438+brandonarivera@users.noreply.github.com>
2020-02-18 11:31:30 -0600

    Fixing Naming Issues and 500.3 Start up error
    Merged into new Project sharing Mapper name
    Fixing start up error allowing web app to be hosted.

---
+++ Mappers/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mappers.Data;
+using Mappers.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Mappers.Areas.Identity.IdentityHostingStartup))]
+namespace Mappers.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
